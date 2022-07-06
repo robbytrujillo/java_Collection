2e3559b401f55ec79ad6dbe17f39aa4453f977d2
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2022-07-06 19:44:40 +0700

    Up Collection Interface

--- src/programmer/zaman/now/collection/CollectionApp.java
+++ src/programmer/zaman/now/collection/CollectionApp.java
@@ -13,12 +13,29 @@ public class CollectionApp {
         collection.add("Robby");
         collection.add("Ilham");
         collection.add("Kusuma");
-        collection.addAll(Arrays.asList("Programmer", "Zaman", "Now" )); //cara satu
+        collection.addAll(Arrays.asList("Programmer", "Zaman", "Now")); //cara satu
         collection.addAll(List.of("Programmer", "Zaman", "Now")); //cara dua
 
-        for (var name : collection) {
+        for (var name : collection) { //cek data collection
             System.out.println(name);
         }
 
+        System.out.println("REMOVE");
+
+        collection.remove("Kusuma"); //Menghapus data collection
+        collection.removeAll(List.of("Zaman", "Now")); //Menghapus sumua data pilihan collection
+
+        for (var value : collection) {
+            System.out.println(value);
+        }
+
+        System.out.println(
+                collection.contains("Ilham")
+        );
+
+        System.out.println(collection.containsAll(List.of("Kusuma", "Progammmer")));
+
+        System.out.println(collection.size());
     }
+
 }
