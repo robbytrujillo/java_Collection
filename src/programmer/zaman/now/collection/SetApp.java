ed2a0efb6f6f9291a564754ba1525f63fa017c0d
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2022-07-09 10:07:27 +0700

    Up Set Interface

--- src/programmer/zaman/now/collection/SetApp.java
+++ src/programmer/zaman/now/collection/SetApp.java
@@ -1,12 +1,16 @@
 package programmer.zaman.now.collection;
 
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Set;
 
 public class SetApp {
     public static void main(String[] args) {
 
-        Set<String> names = new HashSet<>();
+        //Set<String> names = new HashSet<>();//hasilnya tidak berurut
+        Set<String> names = new LinkedHashSet<>(); //hasilnya berurut,
+        // karena didalemnya ada struktur data double linkedlist untuk memaintenance
+
         names.add("Robby");
         names.add("Ilham");
         names.add("Kusuma");
@@ -18,5 +22,8 @@ public class SetApp {
             System.out.println(name);
         }
     }
+//perbedaan list dan set yaitu,
+// jika butuh list yaitu ketika kita tidak peduli duplikat atu gak struktur datanya
+//jika set yaitu ketika data yang di dalam set itu unique yaitu yang tidak ada duplicate
 
 }
