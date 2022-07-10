ab1bfbda32b117a8439a5372262591c8da8ec3e9
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2022-07-10 18:34:55 +0700

    Immutable Set

---
+++ src/programmer/zaman/now/collection/ImmutableSetApp.java
@@ -0,0 +1,25 @@
+package programmer.zaman.now.collection;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+public class ImmutableSetApp {
+    public static void main(String[] args) {
+
+        Set<String> empty = Collections.emptySet();
+        Set<String> one = Collections.singleton("Robby");
+
+        Set<String> mutable = new HashSet<>();
+        mutable.add("Robby");
+        mutable.add("Ilham");
+        Set<String> immutable = Collections.unmodifiableList(mutable);
+
+        Set<String> set = Set.of("Robby", "Ilham");
+
+        //set.add("Robby");
+        //set.remove("Robby");
+
+
+    }
+}
