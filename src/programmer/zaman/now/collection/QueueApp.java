a700e8b7bcad110d2066f5f0c779e8be3f33b448
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2022-07-13 14:47:39 +0700

    Queue Interface Array, Priority, and Linkedlist Deque

--- src/programmer/zaman/now/collection/QueueApp.java
+++ src/programmer/zaman/now/collection/QueueApp.java
@@ -1,20 +1,31 @@
 package programmer.zaman.now.collection;
 
 import java.util.ArrayDeque;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
 import java.util.Queue;
 
 public class QueueApp {
     public static void main(String[] args) {
 
-        Queue<String> queue = new ArrayDeque<>(10);
+       // Queue<String> queue = new ArrayDeque<>(10);
+        //Queue<String> queue = new PriorityQueue<>(); //berurut sesuai abjad
+        Queue<String> queue = new LinkedList<>();
 
-        for (int i = 0; i < 20; i++) {
-            queue.add(String.valueOf(i));
-        }
+//        for (int i = 0; i < 20; i++) {
+//            queue.add(String.valueOf(i));
+//        }
 
+        queue.add("Robby");
+        queue.add("Ilham");
+        queue.add("Kusuma");
+
+        //poll itu menghapus dan mengambil data yang paling depan
         for (String next = queue.poll(); next != null; next= queue.poll()) {
             System.out.println(next);
         }
+
+        System.out.println(queue.size());
 
     }
 }
