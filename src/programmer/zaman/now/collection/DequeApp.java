6ed5e0925022a43e60e9298569e17341b6662c96
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2022-07-15 07:05:58 +0700

    Deque LinkedList

--- src/programmer/zaman/now/collection/DequeApp.java
+++ src/programmer/zaman/now/collection/DequeApp.java
@@ -1,8 +1,20 @@
 package programmer.zaman.now.collection;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
 public class DequeApp {
     public static void main(String[] args) {
 
+        Deque<String> deque = new LinkedList<>();
+
+        deque.offerFirst("Robby");
+        deque.offerFirst("Ilham");
+        deque.offerFirst("Kusuma");
+
+        for (var value : deque) {
+            System.out.println(value);
+        }
     }
 
 }
