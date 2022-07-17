cb101d9dee4679af98bf2dd5e9880dc6be7f0df9
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2022-07-18 00:00:59 +0700

    Hash Map

--- src/programmer/zaman/now/collection/HashMapApp.java
+++ src/programmer/zaman/now/collection/HashMapApp.java
@@ -1,7 +1,19 @@
 package programmer.zaman.now.collection;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class HashMapApp {
     public static void main(String[] args) {
+
+        Map<String, String> map = new HashMap<>();
+        map.put("name.first", "Robby");
+        map.put("name.middle", "Ilham");
+        map.put("name.last", "Kusuma");
+
+        System.out.println(map.get("name.first"));
+        System.out.println(map.get("name.middle"));
+        System.out.println(map.get("name.last"));
 
     }
 }
