0ed32860da6a8bd46787cef13f67d2e29420cba6
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2022-07-29 07:09:11 +0700

    Abstract Collection

---
+++ src/programmer/zaman/now/collection/SingleQueueApp.java
@@ -0,0 +1,26 @@
+package programmer.zaman.now.collection;
+
+import programmer.zaman.now.collection.collection.SingleQueue;
+
+import java.util.Queue;
+
+public class SingleQueueApp {
+    public static void main(String[] args) {
+
+        Queue<String> queue = new SingleQueue<>();
+        System.out.println(queue.size());
+
+        System.out.println(queue.offer("Robby"));
+        System.out.println(queue.offer("Ilham"));
+        System.out.println(queue.offer("Kusuma"));
+
+        System.out.println(queue.size());
+
+        System.out.println(queue.peek());
+        System.out.println(queue.poll());
+        System.out.println(queue.poll());
+        System.out.println(queue.poll());
+
+        System.out.println(queue.size());
+    }
+}
